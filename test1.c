c4ffe2662d70028754540710ae6ad84d62bc50c0
dzikranaufal <bdgbgd123@gmail.com>
2025-11-29 10:59:59 +0700

    nambahin for

--- test1.c
+++ test1.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
 int main () {
-printf ("hello world");
-
+    printf ("hello world");
+    printf ("test");
+    for (int i = 0; i < 10; i++){
+        printf("%d", i);
+    }
+    
 }
