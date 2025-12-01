29c4145cffa782106533c8a7ca5201f771259b40
kdghgz <kaylahaniifa@gmail.com>
2025-12-01 12:06:47 +0700

    tambahin for

--- test1.c
+++ test1.c
@@ -5,7 +5,7 @@ int main () {
     printf("hello");
     printf("hi");
     // banyak teuinh cuk printf nya jir
-    for(int i = 0; i>10;i++) printf("hi);
+    for(int i = 0; i>10;i++) printf("hi");
 
 
 }
